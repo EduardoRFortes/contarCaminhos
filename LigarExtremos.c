eac366d1a8dd19a369cb981011b12c42c28fb235
Eduardo Rodrigues Fortes <rodriguesfortes179@gmail.com>
2025-05-07 11:36:45 -0300

    Update LigarExtremos.c

--- LigarExtremos.c
+++ LigarExtremos.c
@@ -17,13 +17,10 @@ int contaCaminhos(int i, int j, int somaAtual, int n, int matriz[10][10]){
 int main(){
     int i, j, matriz[10][10], n;
     srand(time(NULL));
-    
-    // Preenchendo a matriz com valores aleatórios de 0 a 10
     for(i = 0; i < 10; i++){
         for(j = 0; j < 10; j++)
             matriz[i][j] = rand() % 11;
     }
-
     printf("Matriz gerada:\n");
     for(i = 0; i < 10; i++){
         for(j = 0; j < 10; j++){
@@ -31,13 +28,9 @@ int main(){
         }
         printf("\n");
     }
-
     printf("Informe um valor menor que 150: ");
     scanf("%d", &n);
-
     int total = contaCaminhos(0, 0, 0, n, matriz);
-
     printf("Total de caminhos com soma %d: %d\n", n, total);
-
     return 0;
 }
